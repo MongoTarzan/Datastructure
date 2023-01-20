fe4e3fbec95b029003ad84cedd43f8de1e501046
MongoTarzan <tizian.k@gmx.at>
2023-01-20 15:05:32 +0100

    added quicksort algorithm

---
+++ QuickSort/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class QuickSort
+{
+    static void Main(string[] args) {}
+
+    static void QuicksortAlgorithm(int[] arr, int left, int right)
+    {
+        if (left < right)
+        {
+            int teiler = Partition(arr, left, right);
+
+            if (teiler > 1)
+            {
+                QuicksortAlgorithm(arr, left, teiler - 1);
+            }
+            if (teiler + 1 < right)
+            {
+                QuicksortAlgorithm(arr, teiler + 1, right);
+            }
+        }
+    }
+
+    static int Partition(int[] arr, int left, int right)
+    {
+        int teiler = arr[left];
+        while (true)
+        {
+            while (arr[left] < teiler)
+            {
+                left++;
+            }
+            while (arr[right] > teiler)
+            {
+                right--;
+            }
+            if (left < right)
+            {
+                if (arr[left] == arr[right]) return right;
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+            }
+            else
+            {
+                return right;
+            }
+        }
+    }
+}
